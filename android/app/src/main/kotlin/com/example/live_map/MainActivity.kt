f7d1bd96a10f8b6c21d80281e2cae0a99767327a
Manish Prakash <manishprakash057@gmail.com>
2022-07-13 09:30:42 +0530

    live map

---
+++ android/app/src/main/kotlin/com/example/live_map/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.live_map
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/google_map_api.dart
@@ -0,0 +1,5 @@
+class GoogleMapApi {
+  String _url = "AIzaSyD3xbBXk4Nlz5CSgfnK2_0KEDKSTOo71sc";
+
+  String get url => _url;
+}
